0e0a68251795cdea3bb1fadf047ace7994671289
hansvb_immalle <hans.vanbroeckhoven@immalle.be>
2017-03-23 13:36:11 +0100

    Change descriptions

--- MonsterGame/Program.cs
+++ MonsterGame/Program.cs
@@ -10,11 +10,11 @@ namespace MonsterGame
     {
         static void Main(string[] args)
         {
-            Hero hero = new Hero("Ikke", 200, 80);
+            Hero hero = new Hero("Immahero", 200, 80);
 
             var hall = new Room("hall", description: "A very narrow hallway...");
             var kitchen = new Room("kitchen", 15, "It smells like something died in here...");
-            var office = new Room("office", 10, "It's a long time ago somebody was actually working in here...");
+            var office = new Room("office", 10, "It seems like a really long time ago somebody was working in here.\nThere is a broken computer-screen on a desk.\nThe seat of the chair has been largely eaten by mice.");
             var garden = new Room("garden", 5, "No sign of grass in this garden.\nIt contains only huge weeds.\nSome are longer than you.");
 
             garden.Attach(hall);
